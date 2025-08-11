75782909ce08256225a4776abd076e1e69d5767f
tobi <Tobias20.3@hotmail.de>
2025-08-11 16:17:57 +0200

    Update metric calculation
    paper seems to use weighted f1/precision

--- final_experiments/evaluate.py
+++ final_experiments/evaluate.py
@@ -2,60 +2,46 @@ import numpy as np
 
 from cfire.cfire_module import CFIRE
 from .metrics import get_rule_size, get_literal_count, get_unique_literal_count, max_offdiag_iou, mean_offdiag_iou, get_class_iou_matrix_mc
-from sklearn.metrics import precision_recall_fscore_support
+from sklearn.metrics import precision_recall_fscore_support, f1_score, accuracy_score
+
 
 def evaluate_cfire(
-    cfire: CFIRE,
-    X_val: np.ndarray,
-    X_test: np.ndarray,
-    y_val_model_pred: np.ndarray,
-    y_test_model_pred: np.ndarray,
+        cfire: CFIRE,
+        X_val: np.ndarray,
+        X_test: np.ndarray,
+        y_val_model: np.ndarray,
+        y_test_model: np.ndarray,
 ) -> dict:
+    results = {}
 
     y_val_cfire = cfire(X_val)
     y_test_cfire = cfire(X_test)
 
-    val_acc = float((y_val_cfire == y_val_model_pred).mean())
-    test_acc = float((y_test_cfire == y_test_model_pred).mean())
+    datasets = {
+        "val": (y_val_model, y_val_cfire),
+        "test": (y_test_model, y_test_cfire),
+    }
+    for prefix, (y_true, y_pred) in datasets.items():
+        results[f"{prefix}_acc"] = accuracy_score(y_true, y_pred)
+        for avg_type in ["macro", "weighted"]:
+            precision, recall, f1, _ = precision_recall_fscore_support(
+                y_true, y_pred, average=avg_type, zero_division=0
+            )
+            results[f"{prefix}_precision_{avg_type}"] = precision
+            results[f"{prefix}_recall_{avg_type}"] = recall
+            results[f"{prefix}_f1_{avg_type}"] = f1
 
-    val_prec, val_rec, val_f1, _ = precision_recall_fscore_support(
-        y_val_model_pred,
-        y_val_cfire,
-        average="macro", # un‑weighted mean over classes
-        zero_division=0, # 0 if undefined
-    )
-
-    test_prec, test_rec, test_f1, _ = precision_recall_fscore_support(
-        y_test_model_pred,
-        y_test_cfire,
-        average="macro",
-        zero_division=0,
-    )
-
-    rule_size = get_rule_size(cfire.dnf.rules)
-    literal_count = get_literal_count(cfire.dnf.rules)
-    unique_literal_count =  get_unique_literal_count(cfire.dnf.rules)
-    class_iou_matrix = get_class_iou_matrix_mc(cfire.dnf.rules)
-    max_iou = max_offdiag_iou(class_iou_matrix)
-    mean_iou = mean_offdiag_iou(class_iou_matrix)
+    rules = cfire.dnf.rules
+    results["rule_count"] = len(rules)
+    results["rule_size"] = get_rule_size(rules)
+    results["literal_count"] = get_literal_count(rules)
+    results["unique_literal_count"] = get_unique_literal_count(rules)
+    class_iou_matrix = get_class_iou_matrix_mc(rules)
+    results["max_iou"] = max_offdiag_iou(class_iou_matrix)
+    results["mean_iou"] = mean_offdiag_iou(class_iou_matrix)
 
     # TODO: add more metrics, e.g:
-    #   - are there any more cfire metrics from papaer we might want to include?
+    #   - are there any more cfire metrics from paper we might want to include?
     #   - pruning "metrics"
 
-    return {
-        "val_acc": val_acc,
-        "test_acc": test_acc,
-        "val_precision": val_prec,
-        "val_recall": val_rec,
-        "val_f1": val_f1,
-        "test_precision": test_prec,
-        "test_recall": test_rec,
-        "test_f1": test_f1,
-
-        "rule_size": rule_size,
-        "literal_count": literal_count,
-        "unique_literal_count": unique_literal_count,
-        "max_iou": max_iou,
-        "mean_iou": mean_iou,
-    }
+    return results
